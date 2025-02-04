ad05e55e91ea236dda554b447e0732b017cd5ef6
L Abinesh <abineshl653@gmail.com>
2025-02-04 20:40:21 +0530

    Enemy Movement Corrected

--- Assets/Scripts/WaveSpwaner.cs
+++ Assets/Scripts/WaveSpwaner.cs
@@ -8,6 +8,7 @@ public class Wave
     public GameObject[] typesOfEnemies;
     public float minSpwanInterval;
     public float maxSpwanInterval;
+    public float movementSpeed;
 }
 
 
@@ -22,10 +23,14 @@ public class WaveSpwaner : MonoBehaviour
     private Wave currentWave;
     private int currentWaveNumber;
     private float nextSpwanTime;
+    private GameObject spawnedEnemy;
+    private Rigidbody2D spawnedEnemyRb;
+    private GameObject randomEnemy;
+    private Transform randomPoint;
 
     private bool canSpwan = true;
-    
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,14 +51,22 @@ public class WaveSpwaner : MonoBehaviour
 
         if (canSpwan && nextSpwanTime < Time.time)
         {
-            GameObject randomEnemy = currentWave.typesOfEnemies[Random.Range(0, currentWave.typesOfEnemies.Length)];
-            Transform randomPoint = spwanPoints[Random.Range(0, spwanPoints.Length)];
+            randomEnemy = currentWave.typesOfEnemies[Random.Range(0, currentWave.typesOfEnemies.Length)];
+
+
+            randomPoint = spwanPoints[Random.Range(0, spwanPoints.Length)];
+
             if (currentWaveNumber == 1)
             {
                 randomPoint = jetSpwan;
             }
 
-            Instantiate(randomEnemy, randomPoint.position, randomPoint.rotation);
+            spawnedEnemy = Instantiate(randomEnemy, randomPoint.position, randomPoint.rotation);
+
+            spawnedEnemyRb = spawnedEnemy.GetComponent<Rigidbody2D>();
+
+            EnemyMovments();
+
             currentWave.NumOfEnemies--;
             nextSpwanTime = Time.time + Random.Range(currentWave.minSpwanInterval, currentWave.maxSpwanInterval);
 
@@ -74,6 +87,14 @@ public class WaveSpwaner : MonoBehaviour
     {
         currentWaveNumber++;
         canSpwan = true;
+    }
+
+    void EnemyMovments()
+    {
+        if (spawnedEnemyRb != null)
+        {
+            spawnedEnemyRb.velocity = randomPoint.right * currentWave.movementSpeed;
+        }
     }
 
 }
